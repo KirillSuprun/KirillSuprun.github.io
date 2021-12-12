cf67438aea0f5225d7b20a91d2dadb85a9e815cb
Kirill <kir.suprun@gmail.com>
2021-12-12 19:34:58 +0200

    JS1

---
+++ 18Homework_JS/first_JS/assets/JS/app1.js
@@ -0,0 +1,7 @@
+let sideSquare = +prompt("Enter length of the side of the square (сm)");
+console.log(`Side of square - ${sideSquare}`);
+
+let perimeter = sideSquare * 4;
+console.log(`Perimeter - ${perimeter} cm`);
+
+alert(`perimeter of square - ${perimeter} cm`);
