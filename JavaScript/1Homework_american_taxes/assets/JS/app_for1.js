087d8beb05eb12ee56a5070a5f544ef6f0ebce64
Kirill <kir.suprun@gmail.com>
2022-01-18 22:56:19 +0200

    Update app_for1.js

--- JavaScript/1Homework_american_taxes/assets/JS/app_for1.js
+++ JavaScript/1Homework_american_taxes/assets/JS/app_for1.js
@@ -7,7 +7,7 @@ let federalIncomeTax = 0;
 let sumAmountDeduction = sumIncome - deduction;
 
 
-console.log(`Доход с вычетом подоходного налога - ${sumAmountDeduction}`);
+console.log(`Сумма для расчета налогов со стандартным вычетом  - ${sumAmountDeduction} USD`);
 
 let arrTaxLimit = [0, 9950, 40525, 86376, 164925, 209425, 523600, "Very Big Number",];
 let taxPercent = [0.1, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37]
