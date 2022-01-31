0b7decb0614915c3d3ea9659c1554a1d77d0ef5e
Kirill <kir.suprun@gmail.com>
2022-01-31 20:21:47 +0200

    JS3 task 1

---
+++ JavaScript/3Homework_IPN_and_cards/JS1/assets/JS/app.js
@@ -0,0 +1,24 @@
+
+    let inn = +prompt(`Введите Ваш ИНН для проверки (10цифр)`);
+    console.log("ИНН", inn);
+
+    console.log(inn.length);
+
+if(inn.length != 10) {
+    alert("Вы ввели некоректный ИНН");
+    
+} else {
+    alert(`Проверка ИНН`)
+
+
+
+
+
+
+
+
+
+
+
+
+}
