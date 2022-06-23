5add5b90384d31e13d09fff09f08b8d66a550524
Kirill <kir.suprun@gmail.com>
2022-06-23 21:18:58 +0300

    add spinner 23/06/2022

--- JavaScript/4Homework_Document_Object_model(DOM)/assets/JS/app.js
+++ JavaScript/4Homework_Document_Object_model(DOM)/assets/JS/app.js
@@ -44,14 +44,6 @@ let arrCountries = countries.map(j => ({
 
 console.log(`arrCountries`, arrCountries);
 
-// for(let i = 0; i < arrCountries.length; i++){
-//     if(arrCountries[i].currencies != undefined){
-//         arrCountries[i].currencies = Object.keys(arrCountries[i].currencies).join('');
-//     } else{
-
-//     arrCountries[i].currencies = {};
-//    }
-// };
 
 for (let i = 0; i < arrCountries.length; i++ ) {
     if (arrCountries[i].currencies == undefined) {
@@ -75,20 +67,6 @@ console.log(`arrCountries.length`, arrCountries.length);
 
 
 
-        
-//         arrCountries[j].currencies = arrCountries[j].currencies.toString();
-//         console.log(arrCountries[j].currencies `arrCountries[j].currencies.toString();`);
-
-//         if (arrCountries[j].currencies !== undefined){
-//             console.log(`work!`);
-//             ;
-//         } 
-//     }
-// }
-
-
-
-
 console.log(`arrAnswer`, arrAnswer);
 
 
@@ -107,128 +85,3 @@ cardsForRate.innerHTML = arrAnswer.map(answer  => `<div class="p-2 "> <div id=""
                                                  </div> </div>`)
                                                  .join('');
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
-// const URL2 = 'https://restcountries.com/v3.1/all';
-
-// let dataRate = await fetch(URL);
-//     dataRate = await dataRate.json();
-//     // dataRate = dataRate;
-
-// let countries = await fetch(URL2);
-//     countries = await countries.json();
-//     // сountries = сountries;
-
-// console.log(`Курсы`, dataRate);
-// console.log(`Страны`, countries);
-
-// // достаем данные из JSON файлов
-
-// let cardsForRate = spaceForCards; // cardsForRate = document.getElementById(`spaceForCards`);
-// console.log(cardsForRate);
-
-// let flags1 = '';
-// let counter = 0
-// let flagString = '';
-
-// cardsForRate.innerHTML = dataRate.map((rates) => `<div id="" class="card text-center">
-//                                                 <div class="card-body">
-//                                                 <h5 class="card-title"> ${rates.txt}, ${rates.cc} </h5>
-//                                                 <p class="card-text"> Курси валют: ${(Math.round(rates.rate*100))/100}грн. </p>
-//                                                 <p class="card-text"> Країни, де використовують:  </p>
-//                                                 <p id="${rates.cc}" class="card-text"> ${countries.map((flagsPNG) => {
-//                                                                        console.log(flagString = flagsPNG.stringify(flagsPNG.currencies));
-
-//                                                     /* flags1 = ''; */ console.log(flagsPNG.currencies == rates.cc)
-//                                                     if(flagsPNG.currencies == rates.cc){ 
-//                                                     counter++
-//                                                     console.log(counter);
-//                                                     console.log(flags1);
-//                                                     flags1 += ' ' + flagsPNG.flags.png;  
-//                                                     return "<img src="+ flags1 + ' alt=""' } 
-//                                                     else { return flags1}  }) }
-                                                     
-                                                        
-//                                                         </p>
-//                                                 // </div>
-//                                                 // </div>` 
-                                                
-                                                
-//                                                 // for(let i = 0; i < flagsPNG.lenght; i++){
-                                                
-                                                
-//                                                 ).join();
-
-// ///////////////////////////////////////////////////////////////////////////////////////////////// Добавили карточку, кроме флагов
-
-// // ${countries.map((flagsPNG) => {
-// //     flags1 = '';
-// //     if(flagsPNG.currencies = rates.cc){ 
-// //     console.log(flags1)
-// //     flags1 += ' ' + flagsPNG.flags.png  
-// //     return flags1  }}) } 
-
-
-
-
-// // let flags;
-// // --------------------------
-// // flags.innerHTML = сountries.map((flagsPNG) => {
-//                                                     //     if(rates.cc == flagsPNG.currencies){
-                                            
-//                                                     //             flags += `<img src="${flags.png}" alt=""></img>`;
-//                                                     //             return flags
-//                                                     //         }
-                                                            
-//                                                     //      }
-//                                                     //   )} 
-// // ---------------------------
-
-// // let flags = rates.cc;
-// // console.log(`forFlag`, forFlag);
-
-// // flags.insertAdjacentElement(position, element)
-// // let flags = rate.cc
-
-// // flags.insertAdjacentElement(beforeEnd, `<img src="${flags.png}" alt=""></img>`) = сountries.map((flagsPNG) => {
-// //     if(rates.cc == flagsPNG.currencies){
-     
-// //      }
-// //   })
-
-// function calculateDistance(UserLat, UserLong, TSOLat, TSOLong)
-// function calculateDistance(latA, longA, latB, longB) {
-//     var EARTH_RADIUS = 6372795;
- 
-//     lat1 = latA * Math.PI / 180;
-//     lat2 = latB * Math.PI / 180;
-//     long1 = longA * Math.PI / 180;
-//     long2 = longB * Math.PI / 180;
-  
-//     cl1 = Math.cos(lat1);
-//     cl2 = Math.cos(lat2);
-//     sl1 = Math.sin(lat1);
-//     sl2 = Math.sin(lat2);
-//     delta = long2 - long1;
-//     cdelta = Math.cos(delta);
-//     sdelta = Math.sin(delta);
-  
-//     y = Math.sqrt(Math.pow(cl2 * sdelta, 2) + Math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2));
-//     x = sl1 * sl2 + cl1 * cl2 * cdelta;
-  
-//     ad = Math.atan2(y, x);
-//     dist = Math.ceil(ad * EARTH_RADIUS);
-  
-//     return dist;
-// }
